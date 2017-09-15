fdadf6c2a67ac44f5253530c36bf720779188be3
John Cook <john.b.cook05@gmail.com>
2017-09-15 09:05:45 -0600

    updated .Net Core install

--- jenkins-ubuntu-fresh/user-data.sh
+++ jenkins-ubuntu-fresh/user-data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 
-apt-get -y --force-yes upgrade 
+apt-get -yq --force-yes upgrade 
 
 apt-get -y --force-yes install python-minimal 
